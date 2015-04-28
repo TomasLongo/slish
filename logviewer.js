0b5fae640e35e5a91bf0172a9f288114e3fdac9d
Tomas Longo <longo.tomas@googlemail.com>
2015-04-28 09:30:57 +0200

    Viewer now displays new lines added to the file.

--- logviewer.js
+++ logviewer.js
@@ -1,5 +1,6 @@
 var fs = require('fs'),
-    util = require('util');
+    util = require('util'),
+    logger = require('BleedingTea');
 
 /**
   In this first version we will poll the file every x millis.
@@ -19,6 +20,9 @@ var initialBytesToRead = 500;
 var bytesToRead = initialBytesToRead;
 var lastSize = 0;
 
+var lastPosition = 0;
+
+logger.active = false;
 
 /*
   Ermittle die initiale Größe des Files. U.u. ist es noch leer oder hat noch nicht
@@ -30,6 +34,18 @@ function init(file) {
       initialBytesToRead = stats.size;
       lastSize = stats.size;
     }
+
+    var buffer = new Buffer(initialBytesToRead);
+    fs.open(file, "r", function(error, fd) {
+      getLastLines(fd, stats.size, initialBytesToRead, function(error, lastLines) {
+        console.log(lastLines);
+      });
+
+      // fs.read(fd, buffer, 0, initialBytesToRead, stats.size - initialBytesToRead, function(error, bRead, buffer) {
+      //   logger.info("Read " + bRead + " bytes from " + file);
+      //   console.log(String(buffer));
+      // });
+    });
   });
 }
 
@@ -38,11 +54,47 @@ function poll(file) {
     var currentSize = stats.size;
     var delta = currentSize - lastSize;
     lastSize = currentSize;
-    console.log("File has additional " + delta + " bytes");
+    logger.info("File has additional " + delta + " bytes");
+
+    var buffer = new Buffer(delta);
+    if (delta > 0) {
+      fs.open(file, "r", function(error, fd) {
+        fs.read(fd, buffer, 0, delta, currentSize - delta, function(error, bRead, buffer) {
+          logger.info("Read " + bRead + " bytes from " + file);
+          var toAppend = String(buffer);
+          console.log(toAppend.slice(0, toAppend.length-1));
+        });
+      });
+    }
+  });
+}
+
+/**
+  Returns the last lines of a file denoted by fd.
+
+  params:
+    fd          The filedescriptor of the file
+    fileSize    The size of the file
+    byteToRead  The length of the file tail
+    callback    function(error, lastLines)
+*/
+function getLastLines(fd, fileSize, bytesToRead, callback) {
+  var buffer = new Buffer(bytesToRead);
+  fs.read(fd, buffer, 0, bytesToRead, fileSize - bytesToRead, function(error, bRead, buffer) {
+    var bufferString = String(buffer);
+    var posToStartRead = -1;
+    for (i = 0; i < bufferString.length; i++) {
+      if (bufferString.charAt(i) == "\n") {
+        posToStartRead = i;
+        break;
+      }
+    }
+    callback(null, bufferString.slice(posToStartRead+1, bufferString.length));
   });
 }
 
 
+
 init(file);
 
-setInterval(poll, 5000, file);
+setInterval(poll, 2000, file);
